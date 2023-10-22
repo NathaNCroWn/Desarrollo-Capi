ec67e6e3886637205ef1e40e749326f28b3e4187
NathanCrow <Jhonatandlopez@gmail.com>
2023-10-22 13:14:24 -0500

    correccion de modulos back

--- cotizacion/urls.py
+++ cotizacion/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('get/', views.get_Cotizacion),
-    path('create/', views.register_Cotizacion),
+    path('create/', views.create_Cotizacion),
     path('update/<int:id>', views.update_Cotizacion),
     path('delete/<int:id>', views.delete_Cotizacion)
 ]
--- productos/models.py
+++ productos/models.py
@@ -4,7 +4,7 @@ from usuario.models import Usuario
 class Productos(models.Model):
     UserProduct= models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True)
     productName= models.CharField(max_length=150)
-    productImg= models.ImageField(null=True)
+    productImg= models.ImageField(null=True, blank=True)
     productDescription= models.CharField(max_length=1500)
     productDescriptionSimple= models.CharField(max_length=1500)
     price=models.FloatField(null=True)
